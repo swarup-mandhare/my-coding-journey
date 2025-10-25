bea195c1fde8110a765db413f3ec101150f92019
Swarup Mandhare <163109297+swarup-mandhare@users.noreply.github.com>
2025-10-26 02:09:22 +0530

    Add files via upload

--- practice6.py
+++ practice6.py
@@ -1,1 +1,15 @@
-# wap to ask user to enter names of their 3 fav movies
+# wap to ask user to enter names of their 3 fav movies and store them as a list
+name = input("Enter you name: ")
+print("Hello",name,"!")
+movies = []
+
+mov1 = input("Enter the first movie name: ")
+movies.append(mov1)
+
+mov2 = input("Enter the second movie name: ")
+movies.append(mov2)
+
+mov3 = input("Enter the third movie name:")
+movies.append(mov3)
+
+print(movies)
